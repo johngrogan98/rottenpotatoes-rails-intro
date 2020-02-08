1d7539d75512604411f4286f9b9ac4fc26a8eac9
EC2 Default User <ec2-user@ip-172-31-81-180.ec2.internal>
2020-02-08 03:15:04 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,6 +4,7 @@ class MoviesController < ApplicationController
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
 
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
