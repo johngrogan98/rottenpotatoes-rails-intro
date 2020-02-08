62463d943f6822d7ad95b14a57227cab81461e43
EC2 Default User <ec2-user@ip-172-31-81-180.ec2.internal>
2020-02-08 02:46:45 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    
+    #checks if a sort has been selected
 		if params.key?(:sort_by)
 			session[:sort_by] = params[:sort_by]
 		elsif session.key?(:sort_by)
@@ -19,22 +19,28 @@ class MoviesController < ApplicationController
 			redirect_to movies_path(params) and return
 		end
 		
+		#highlight if sort has been selected
 		sort_by = session[:sort_by]
 		@hilite = sort_by
-		@all_ratings = Movie.all_ratings
-		@checked_ratings = @all_ratings
 		
+	  #set checked ratings to all ratings
+		@checked_ratings = ['G','PG','PG-13','R']
+		
+		#check the ratings that have been selected
 		if params.key?(:ratings)
 			session[:ratings] = params[:ratings]
+			#change the checked ratings if necessary
 			@checked_ratings = params[:ratings].keys
 		elsif session.key?(:ratings)
 			params[:ratings] = session[:ratings]
 			redirect_to movies_path(params) and return
 		end
 		
+		#get a list of movies in the selected ratings
 		movie = Movie.with_ratings(@checked_ratings)
+		
+		#sort those based on the sort variable
     @movies = movie.order(sort_by)
-    
   end
 
   def new
